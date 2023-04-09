c2917347dfd5fe36dd30c57edbd6b6e66ee2ee51
frostBite <55104069+renbe@users.noreply.github.com>
2023-04-10 00:44:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true, // Set to true to enable autoplay
+      autoplaySpeed: 2000, // Set the autoplay speed (in milliseconds)
+      arrows: true, // Set to true to show navigation arrows
+      dots: true // Set to true to show pagination dots
+    });
+  });
+ 
+    
